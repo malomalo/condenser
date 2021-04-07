2a540e2d1d7c2009f1fb1291cbc4eaa9efc064e7
Ben Ehmke <benehmke@gmail.com>
2021-04-07 16:12:05 -0500

    Refactor media_combiner to do combining manually

--- lib/condenser/processors/css_media_combiner_processor.rb
+++ lib/condenser/processors/css_media_combiner_processor.rb
@@ -1,24 +1,46 @@
 class Condenser::CSSMediaCombinerProcessor < Condenser::NodeProcessor
   
-  def self.setup(environment)
-    require "css_parser" unless defined?(::CssParser)
-  end
-  
   def call(environment, input)
-    output = CssParser::Parser.new
-    parser = CssParser::Parser.new
-    
-    parser.load_string!(input[:source])
-    
-    parser.rules_by_media_query.each do |media_query, rule_sets|
-      rule_sets.each do |rule_set|
-        output.add_rule_set!(rule_set, media_query)
+    buffer = ""
+    query_flag = false
+    media_queries = {}
+    input[:source].split(/(@media)/).each do |result|
+      if query_flag
+        query, rules = result.split("{", 2)
+        
+        rules_buffer = ""
+        bracket_count = 1
+        rules.each_char do |char|
+          if query_flag && char == "{"
+            bracket_count += 1
+          elsif query_flag && char == "}"
+            bracket_count -= 1
+            if bracket_count == 0
+              media_queries[query.strip] ||= ""
+              media_queries[query.strip] += rules_buffer
+              rules_buffer = ""
+              query_flag = false
+              next
+            end
+          end
+          
+          rules_buffer += char
+        end
+        
+        buffer += rules_buffer
+        query_flag = false
+      elsif result == "@media"
+        query_flag = true
+      else
+        buffer += result
       end
-      
-      
     end
     
-    input[:source] = output.to_s
+    media_queries.each do |query, query_buffer|
+      buffer += "@media #{query} {#{query_buffer}}"
+    end
+    
+    input[:source] = buffer
   end
 
 end
--- test/postprocessors/css_media_combiner_test.rb
+++ test/postprocessors/css_media_combiner_test.rb
@@ -35,24 +35,70 @@ class MediaCombinerTest < ActiveSupport::TestCase
     CSS
 
     assert_exported_file 'main.css', 'text/css', <<~FILE
-    .test {
-    display: block;
+    .test{
+      display: block;
     }
-    .test2 {
-    display: block;
+
+
+
+    .test2{
+      display: block;
     }
+
     @media only screen and (max-width: 100px) {
       .test {
         display: inline-block;
       }
+
       .test2 {
         display: inline-block;
       }
-    }
-    @media only screen and (max-width: 500px) {
+    }@media only screen and (max-width: 500px) {
       .test {
         display: inline;
       }
+    }
+    FILE
+  end
+  
+  test 'single line css' do
+    file 'main.css', <<~CSS
+      .test{display: block;} @media only screen and (max-width: 100px) {.test {display: inline-block;}}@media only screen and (max-width: 500px) {.test {display: inline;}}.test2{display: block;}@media only screen and (max-width: 100px) {.test2 {display: inline-block;}}
+    CSS
+    
+    assert_exported_file 'main.css', 'text/css', <<~FILE
+    .test{display: block;} .test2{display: block;}
+    @media only screen and (max-width: 100px) {.test {display: inline-block;}.test2 {display: inline-block;}}@media only screen and (max-width: 500px) {.test {display: inline;}}
+    FILE
+  end
+  
+  
+  test 'keyframes' do
+    file 'main.css', <<~CSS
+    .trobber {
+    animation: throb 1s infinite;
+    }
+    @keyframes throb {
+        0% {
+            opacity: 1;
+        }
+        100%{
+            opacity: 0;
+        }
+    }
+    CSS
+
+    assert_exported_file 'main.css', 'text/css', <<~FILE
+    .trobber {
+    animation: throb 1s infinite;
+    }
+    @keyframes throb {
+        0% {
+            opacity: 1;
+        }
+        100%{
+            opacity: 0;
+        }
     }
     FILE
   end
