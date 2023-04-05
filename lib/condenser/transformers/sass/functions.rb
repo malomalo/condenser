275f7f3fa6849ff6aebfe7293ff3ba1a9d2ee98e
Jon Bracy <jonbracy@gmail.com>
2023-04-05 09:40:29 -0500

    Add DartSass Transformer

---
+++ lib/condenser/transformers/sass/functions.rb
@@ -0,0 +1,119 @@
+# Public: Functions injected into Sass context during Condenser evaluation.
+#
+# This module may be extended to add global functionality to all Condenser
+# Sass environments. Though, scoping your functions to just your environment
+# is preferred.
+#
+# module Condenser::SassProcessor::Functions
+#   def asset_path(path, options = {})
+#   end
+# end
+module Condenser::Sass
+  module Functions
+  
+    # Public: Generate a url for asset path.
+    #
+    # Defaults to Context#asset_path.
+    def asset_path(path, options = {})
+      condenser_context.link_asset(path)
+
+      path = condenser_context.asset_path(path, options)
+      query    = "?#{query}" if query
+      fragment = "##{fragment}" if fragment
+      "#{path}#{query}#{fragment}"
+    end
+
+    # Public: Generate a asset url() link.
+    #
+    # path - String
+    def asset_url(path, options = {})
+      "url(#{asset_path(path, options)})"
+    end
+
+    # Public: Generate url for image path.
+    def image_path(path)
+      asset_path(path, type: :image)
+    end
+
+    # Public: Generate a image url() link.
+    def image_url(path)
+      asset_url(path, type: :image)
+    end
+
+    # Public: Generate url for video path.
+    def video_path(path)
+      asset_path(path, type: :video)
+    end
+
+    # Public: Generate a video url() link.
+    def video_url(path)
+      asset_url(path, type: :video)
+    end
+
+    # Public: Generate url for audio path.
+    def audio_path(path)
+      asset_path(path, type: :audio)
+    end
+
+    # Public: Generate a audio url() link.
+    def audio_url(path)
+      asset_url(path, type: :audio)
+    end
+
+    # Public: Generate url for font path.
+    def font_path(path)
+      asset_path(path, type: :font)
+    end
+
+    # Public: Generate a font url() link.
+    def font_url(path)
+      asset_url(path, type: :font)
+    end
+
+    # Public: Generate url for javascript path.
+    def javascript_path(path)
+      asset_path(path, type: :javascript)
+    end
+
+    # Public: Generate a javascript url() link.
+    def javascript_url(path)
+      asset_url(path, type: :javascript)
+    end
+
+    # Public: Generate url for stylesheet path.
+    def stylesheet_path(path)
+      asset_path(path, type: :stylesheet)
+    end
+
+    # Public: Generate a stylesheet url() link.
+    def stylesheet_url(path)
+      asset_url(path, type: :stylesheet)
+    end
+
+    # Public: Generate a data URI for asset path.
+    def asset_data_url(path)
+      url = condenser_environment.asset_data_uri(path.value)
+      Sass::Script::String.new("url(" + url + ")")
+    end
+
+    protected
+      # Public: The Environment.
+      #
+      # Returns Condenser::Environment.
+      def condenser_context
+        options[:condenser][:context]
+      end
+    
+      def condenser_environment
+        options[:condenser][:environment]
+      end
+
+      # Public: Mutatable set of dependencies.
+      #
+      # Returns a Set.
+      def condenser_dependencies
+        options[:asset][:process_dependencies]
+      end
+
+  end
+end
---
+++ test/transformers/dart_scss_test.rb
@@ -0,0 +1,139 @@
+require 'test_helper'
+
+class CondenserDartSCSSTest < ActiveSupport::TestCase
+
+  def setup
+    super
+    @env.clear_pipeline
+    @env.register_transformer 'text/scss', 'text/css', Condenser::DartScssTransformer
+  end
+
+  test 'find' do
+    file 'test.scss', <<~SCSS
+    body {
+      background-color: green;
+
+      &:hover {
+        background-color: blue;
+      }
+    }
+    SCSS
+
+    assert_file 'test.css', 'text/css', <<~CSS
+    body {
+      background-color: green;
+    }
+    body:hover {
+      background-color: blue;
+    }
+    CSS
+  end
+
+  test 'scss import globing' do
+    file "c/dir/a.scss", "body { color: blue; }"
+    file "c/dir/b.scss", "body { color: green; }"
+
+    file 'c/test.scss', '@import "./dir/*"'
+
+    assert_file 'c/test.css', 'text/css', <<~CSS
+    body {
+      color: blue;
+    }
+
+    body {
+      color: green;
+    }
+    CSS
+
+    file 'c/test2.scss', '@import "c/dir/*"'
+
+    assert_file 'c/test2.css', 'text/css', <<~CSS
+    body {
+      color: blue;
+    }
+
+    body {
+      color: green;
+    }
+    CSS
+  end
+
+  test "url functions" do
+    file 'a.scss', <<~SCSS
+      body {
+        color: green; }
+    SCSS
+
+    file 'test.scss', <<~SCSS
+    @import 'a';
+
+    div {
+       url: asset-url("foo.svg");
+       url: image-url("foo.png");
+       url: video-url("foo.mov");
+       url: audio-url("foo.mp3");
+       url: font-url("foo.woff2");
+       url: font-url("foo.woff");
+       url: javascript-url("foo.js");
+       url: stylesheet-url("foo.css");
+    }
+    SCSS
+
+    file 'foo.svg', ''
+    file 'foo.png', ''
+    file 'foo.mov', ''
+    file 'foo.mp3', ''
+    file 'foo.woff2', ''
+    file 'foo.woff', ''
+    file 'foo.js', ''
+    file 'foo.css', ''
+
+    assert_file 'test.css', 'text/css', <<~CSS
+    body {
+      color: green;
+    }
+
+    div {
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.svg);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.png);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.mov);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.mp3);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.woff2);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.woff);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.js);
+      url: url(/assets/foo-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.css);
+    }
+    CSS
+
+    asset = @env.find('test.css')
+    assert_equal ["a.scss", "foo.svg", "foo.png", "foo.mov", "foo.mp3", "foo.woff2", "foo.woff", "foo.js", "foo.css"], asset.process_dependencies.map(&:filename)
+    assert_equal ["a.scss", "foo.svg", "foo.png", "foo.mov", "foo.mp3", "foo.woff2", "foo.woff", "foo.js", "foo.css"], asset.export_dependencies.map(&:filename)
+  end
+
+  test "sass dependencies" do
+    file 'd.scss', <<~SCSS
+      $secondary-color: #444;
+    SCSS
+
+    file 'a.scss', <<~SCSS
+      @import 'd';
+      $primary-color: #333;
+    SCSS
+
+    file 'b.scss', <<~SCSS
+      body {
+        color: $primary-color;
+      }
+    SCSS
+
+    file 'c.scss', <<~SCSS
+      @import 'a';
+      @import 'b';
+    SCSS
+
+    asset = @env.find('c.css')
+    assert_equal ["a.scss", "d.scss", "b.scss"], asset.process_dependencies.map(&:filename)
+    assert_equal ["a.scss", "d.scss", "b.scss"], asset.export_dependencies.map(&:filename)
+  end
+  
+end
